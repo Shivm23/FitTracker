ba2f3ace6627f0c415552428148f914ecd6926d4
Pierre Lammers <lammers.pierre@gmail.com>
2025-04-23 13:33:01 +0200

    feat: add french language + update dependancy

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -4159,6 +4159,7 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
     return const <Locale>[
       Locale.fromSubtags(languageCode: 'en'),
       Locale.fromSubtags(languageCode: 'de'),
+      Locale.fromSubtags(languageCode: 'fr'),
       Locale.fromSubtags(languageCode: 'tr'),
     ];
   }
