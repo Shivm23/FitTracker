890f9b085a7226ff810c317ef42cfa9d2946b393
b-chollet <benjchollet@gmail.com>
2025-07-15 21:26:39 +0200

    refactor: Refactor MacroNutrientsView

--- lib/features/home/presentation/widgets/macro_nutriments_widget.dart
+++ lib/features/home/presentation/widgets/macro_nutriments_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:opennutritracker/generated/l10n.dart';
 
-class MacroNutrientsView extends StatefulWidget {
+class MacroNutrientsView extends StatelessWidget {
   final double totalCarbsIntake;
   final double totalFatsIntake;
   final double totalProteinsIntake;
@@ -10,131 +10,53 @@ class MacroNutrientsView extends StatefulWidget {
   final double totalFatsGoal;
   final double totalProteinsGoal;
 
-  const MacroNutrientsView(
-      {super.key,
-      required this.totalCarbsIntake,
-      required this.totalFatsIntake,
-      required this.totalProteinsIntake,
-      required this.totalCarbsGoal,
-      required this.totalFatsGoal,
-      required this.totalProteinsGoal});
+  const MacroNutrientsView({
+    super.key,
+    required this.totalCarbsIntake,
+    required this.totalFatsIntake,
+    required this.totalProteinsIntake,
+    required this.totalCarbsGoal,
+    required this.totalFatsGoal,
+    required this.totalProteinsGoal,
+  });
 
-  @override
-  State<MacroNutrientsView> createState() => _MacroNutrientsViewState();
-}
-
-class _MacroNutrientsViewState extends State<MacroNutrientsView> {
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        Row(
-          children: [
-            CircularPercentIndicator(
-              radius: 15.0,
-              lineWidth: 6.0,
-              animation: true,
-              percent: getGoalPercentage(
-                  widget.totalCarbsGoal, widget.totalCarbsIntake),
-              progressColor: Theme.of(context).colorScheme.primary,
-              backgroundColor:
-                  Theme.of(context).colorScheme.primary.withAlpha(50),
-              circularStrokeCap: CircularStrokeCap.round,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Column(
-                children: [
-                  Text(
-                    '${widget.totalCarbsIntake.toInt()}/${widget.totalCarbsGoal.toInt()} g',
-                    style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                        color:
-                            Theme.of(context).colorScheme.onSurface),
-                  ),
-                  Text(
-                    S.of(context).carbsLabel,
-                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                        color:
-                            Theme.of(context).colorScheme.onSurface),
-                  )
-                ],
-              ),
-            )
-          ],
+        _MacronutrientItem(
+          intake: totalCarbsIntake,
+          goal: totalCarbsGoal,
+          label: S.of(context).carbsLabel,
         ),
-        Row(
-          children: [
-            CircularPercentIndicator(
-              radius: 15.0,
-              lineWidth: 6.0,
-              animation: true,
-              percent: getGoalPercentage(
-                  widget.totalFatsGoal, widget.totalFatsIntake),
-              progressColor: Theme.of(context).colorScheme.primary,
-              backgroundColor:
-                  Theme.of(context).colorScheme.primary.withAlpha(50),
-              circularStrokeCap: CircularStrokeCap.round,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Column(
-                children: [
-                  Text(
-                    "${widget.totalFatsIntake.toInt()}/${widget.totalFatsGoal.toInt()} g",
-                    style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                        color:
-                            Theme.of(context).colorScheme.onSurface),
-                  ),
-                  Text(S.of(context).fatLabel,
-                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color: Theme.of(context)
-                              .colorScheme
-                              .onSurface)),
-                ],
-              ),
-            )
-          ],
+        _MacronutrientItem(
+          intake: totalFatsIntake,
+          goal: totalFatsGoal,
+          label: S.of(context).fatLabel,
         ),
-        Row(
-          children: [
-            CircularPercentIndicator(
-              radius: 15.0,
-              lineWidth: 6.0,
-              animation: true,
-              percent: getGoalPercentage(
-                  widget.totalProteinsGoal, widget.totalProteinsIntake),
-              progressColor: Theme.of(context).colorScheme.primary,
-              backgroundColor:
-                  Theme.of(context).colorScheme.primary.withAlpha(50),
-              circularStrokeCap: CircularStrokeCap.round,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Column(
-                children: [
-                  Text(
-                    "${widget.totalProteinsIntake.toInt()}/${widget.totalProteinsGoal.toInt()} g",
-                    style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                        color:
-                            Theme.of(context).colorScheme.onSurface),
-                  ),
-                  Text(
-                    S.of(context).proteinLabel,
-                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                        color:
-                            Theme.of(context).colorScheme.onSurface),
-                  )
-                ],
-              ),
-            )
-          ],
-        )
+        _MacronutrientItem(
+          intake: totalProteinsIntake,
+          goal: totalProteinsGoal,
+          label: S.of(context).proteinLabel,
+        ),
       ],
     );
   }
+}
 
-  double getGoalPercentage(double goal, double supplied) {
+class _MacronutrientItem extends StatelessWidget {
+  final double intake;
+  final double goal;
+  final String label;
+
+  const _MacronutrientItem({
+    required this.intake,
+    required this.goal,
+    required this.label,
+  });
+
+  double _getGoalPercentage(double goal, double supplied) {
     if (supplied <= 0 || goal <= 0) {
       return 0;
     } else if (supplied > goal) {
@@ -142,5 +64,45 @@ class _MacroNutrientsViewState extends State<MacroNutrientsView> {
     } else {
       return supplied / goal;
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final primaryColor = theme.colorScheme.primary;
+    final onSurfaceColor = theme.colorScheme.onSurface;
+
+    return Row(
+      children: [
+        CircularPercentIndicator(
+          radius: 15.0,
+          lineWidth: 6.0,
+          animation: true,
+          percent: _getGoalPercentage(goal, intake),
+          progressColor: primaryColor,
+          backgroundColor: primaryColor.withAlpha(50),
+          circularStrokeCap: CircularStrokeCap.round,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Column(
+            children: [
+              Text(
+                '${intake.toInt()}/${goal.toInt()} g',
+                style: theme.textTheme.titleSmall?.copyWith(
+                  color: onSurfaceColor,
+                ),
+              ),
+              Text(
+                label,
+                style: theme.textTheme.bodyMedium?.copyWith(
+                  color: onSurfaceColor,
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
